32b0cb0a64a089ee82fe60d321f8a57f851b63b8
Andrew <andrew_shliahta@ukr.net>
2021-09-26 20:23:48 +0300

    Задание 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { avatar, name, tag, location, stats } from "./user.json";
-import statisticalData from "./statistical-data.json";
-import friends from "./friends.json";
-import Profile from "./Profile";
-import Statistics from "./Statistics";
-import FriendList from "./FriendList";
+import { avatar, name, tag, location, stats } from './Profile/user.json';
+import statisticalData from './Statistics/statistical-data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export default function App(props) {
-    return (
-        <>
-         <Profile name={name}
-            tag={tag}
-            location={location}
-            avatar={avatar}
-            stats={stats}
-        />
-        <Statistics
-            stats={statisticalData}
-            />
-        <FriendList
-            friends={friends}
-        />
-        
-        </>
-    );
-};
+  return (
+    <>
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
